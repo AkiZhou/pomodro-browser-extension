b7a9433d4308aac5d928eb38e59fe33ac6dbfdb9
Aki Zhou <sza111@sfu.ca>
2020-09-04 22:35:59 -0700

    Added clear whitelist event handler

--- js/popup.js
+++ js/popup.js
@@ -1,2 +1,12 @@
 /***** Enforcing extension API to be cross-browser ******/
 window.browser = (() => { return window.browser || window.chrome })();
+
+
+/***** Main *****/
+const clearList = document.getElementById("clearList");
+clearList.addEventListener(
+    "click",
+    () => {
+        browser.storage.sync.clear();
+    }
+);
