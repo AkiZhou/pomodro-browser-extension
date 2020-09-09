e0409db918f0baa446407fedde1cfb91f4116ec9
Aki Zhou <sza111@sfu.ca>
2020-09-09 00:15:29 -0700

    Updated functions

--- js/background.js
+++ js/background.js
@@ -4,8 +4,11 @@ window.browser = (() => { return window.browser || window.chrome })();
 
 /***** Function definitions *****/
 function extractDomain(url) {
+    // Drop protocol component
     let origin = url.slice(url.search(':')+3);
-    let hostname = origin.slice(0, origin.search('/'));
+    // Extract hostname component
+    let hostname = origin.slice(0, (origin.search('/')==-1) ? origin.length : origin.search('/'));
+    // Extract domain using the Public Suffix List. Details @ https://www.npmjs.com/package/psl
     let parsed = psl.parse(hostname);
     return parsed.domain;
 }
@@ -13,9 +16,9 @@ function extractDomain(url) {
 function newlyWhitelisted(domain) {
     let message = domain + " is not whitelisted.\nWould you like to have access to the domain?";
     if (window.confirm(message)) {
-        let key = domain;
+        //let key = domain;
         let obj = {};
-        obj[key] = true;  // true is a placeholder, value may store something meaningful in the future
+        obj[domain] = true;  // true is a placeholder, value may store something meaningful in the future
         browser.storage.sync.set(obj);
         browser.storage.sync.get((result) => { console.log("New domain added!\nstorage in use: " + Object.keys(result).length + "/512") })
         return true;
@@ -25,6 +28,40 @@ function newlyWhitelisted(domain) {
     }
 }
 
+function blocker(details) {
+    if (typeof details.initiator === "undefined") {
+        let targetDomain = extractDomain(details.url);
+        console.log("Requested access to domain: " + targetDomain + "\nOn tab: " + targetTab);
+
+        // Check if target is whitelisted, all the whitelisted domains are stored in 
+        browser.storage.sync.get(
+            (whitelist) => {
+                if (typeof whitelist[targetDomain] === "undefined") {
+                    console.log("Not whitelisted");
+                    // FIXME: Popup from newlyWhitelisted show up too quickly wait until user finishes typing in the omnibox and hit enter.
+                    // Right now it updates the tab but use {redirectUrl: details.url} when user whitelist the domain aka newlyWhitelisted returns true
+                    if (!newlyWhitelisted(targetDomain)) {
+                        // When browser prerender changes tab ID, subtract 1 from the old ID
+                        // FIXME: The browser console will show runtime error even with valid target ID when using newTargetTab. No problem to functionality so far but need to check.
+                        browser.tabs.update({url: "./blocking.html"});
+                    }
+                    else {
+                        browser.tabs.update({url: details.url});
+                    }
+                }
+                else {
+                    console.log("Domain whitelisted");
+                    return;
+                }
+            }
+        );
+    }
+    else {
+        if (DEBUG) {
+            console.log("Invoked")
+        }
+    }
+}
 
 /***** Main *****/
 const DEBUG = true;
@@ -38,62 +75,7 @@ if (DEBUG) {
 // TODO: Need an event handler for site access through links on whitelisted domains
 browser.webRequest.onBeforeRequest.addListener(
     (details) => {
-        if (typeof details.initiator === "undefined") {
-            // BUG:　extractDomain does not function correctly with URLs containing 2LD. Consider psl package for fix.
-            let targetDomain = extractDomain(details.url);
-            let targetTab = details.tabId;
-            console.log("Requested access to domain: " + targetDomain + "\nOn tab: " + targetTab);
-
-            // Check if target is whitelisted, all the whitelisted domains are stored in 
-            browser.storage.sync.get(
-                (whitelist) => {
-                    if (typeof whitelist[targetDomain] === "undefined") {
-                        console.log("Not whitelisted");
-                        // FIXME: Popup from newlyWhitelisted show up too quickly wait until user finishes typing in the omnibox and hit enter.
-                        // Right now it updates the tab but use {redirectUrl: details.url} when user whitelist the domain aka newlyWhitelisted returns true
-                        if (!newlyWhitelisted(targetDomain)) {
-                            // When browser prerender changes tab ID, subtract 1 from the old ID
-                            // FIXME: The browser console will show runtime error even with valid target ID when using newTargetTab. No problem to functionality so far but need to check.
-                            browser.tabs.get(targetTab,
-                                (result) => {
-                                    if (typeof result === "undefined") {
-                                        let newTargetTab = targetTab - 1;
-                                        console.log("Id-1 @", newTargetTab);
-                                        browser.tabs.update(newTargetTab, { url: "./blocking.html" });
-                                    }
-                                    else {
-                                        browser.tabs.update(targetTab, { url: "./blocking.html" });
-                                    }
-                                }
-                            )
-                        }
-                        else {
-                            browser.tabs.get(targetTab,
-                                (result) => {
-                                    if (typeof result === "undefined") {
-                                        let newTargetTab = targetTab - 1;
-                                        console.log("Id-1 @", newTargetTab);
-                                        browser.tabs.update(newTargetTab, { url: details.url });
-                                    }
-                                    else {
-                                        browser.tabs.update(targetTab, { url: details.url });
-                                    }
-                                }
-                            )
-                        }
-                    }
-                    else {
-                        console.log("Domain whitelisted");
-                        return;
-                    }
-                }
-            );
-        }
-        else {
-            if (DEBUG) {
-                console.log("Invoked")
-            }
-        }
+        blocker(details);
     },
     FILTER,
     ["blocking"]
